c2046bd913baa4e8189a92bee0740c089401fa69
PPakSang <sanghyun-dev@naver.com>
2023-04-24 19:46:36 +0900

    init project

---
+++ src/main/java/com/knu/ntttt_server/NttttServerApplication.java
@@ -0,0 +1,13 @@
+package com.knu.ntttt_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttttServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NttttServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knu/ntttt_server/NttttServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.ntttt_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttttServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
