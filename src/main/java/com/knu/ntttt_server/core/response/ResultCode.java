ace72494690e88930fb7808fa0784e0d003c0c36
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-04-26 11:01:21 +0900

    feat: 커스텀 exception에 별도의 메세지로 설정하여 반환하도록 수정

--- src/main/java/com/knu/ntttt_server/core/response/ResultCode.java
+++ src/main/java/com/knu/ntttt_server/core/response/ResultCode.java
@@ -1,6 +1,7 @@
 package com.knu.ntttt_server.core.response;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 
@@ -19,6 +20,10 @@ public enum ResultCode {
     ResultCode(HttpStatus httpStatus, int code, String message) {
         this.httpStatus = httpStatus;
         this.code = code;
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
     }
 }
