cccd1bea27e2c5de5cde17528eb0d09308517e6a
kayoung0 <kkayoung0@gmail.com>
2023-04-29 21:34:24 +0900

    chore: 공백 추가

--- src/main/java/com/knu/ntttt_server/token/service/PaymentServiceImpl.java
+++ src/main/java/com/knu/ntttt_server/token/service/PaymentServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+
   private final NftService nftService;
   private final TokenRepository tokenRepository;
 
