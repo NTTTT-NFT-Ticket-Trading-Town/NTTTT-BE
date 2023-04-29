1b0a426fac67fe537da8dc452f9e276b7e72430e
kayoung0 <kkayoung0@gmail.com>
2023-04-29 18:19:39 +0900

    Add: PaymentServiceImpl 추가
    유저 지갑 주소, 토큰 id로 토큰 구매 요청하고, 토큰 paymentState를 변경하는 PaymentService 구현체 추가

---
+++ src/main/java/com/knu/ntttt_server/token/service/PaymentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.knu.ntttt_server.token.service;
+
+import com.knu.ntttt_server.core.exception.KnuException;
+import com.knu.ntttt_server.core.response.ResultCode;
+import com.knu.ntttt_server.nft.service.NftService;
+import com.knu.ntttt_server.token.model.PaymentState;
+import com.knu.ntttt_server.token.model.Token;
+import com.knu.ntttt_server.token.repository.TokenRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+  private final NftService nftService;
+  private final TokenRepository tokenRepository;
+
+  /**
+   * tokenID를 가진 token 찾기
+   */
+  Token getToken(Long tokenId) {
+    return tokenRepository.findById(tokenId)
+        .orElseThrow(() -> new KnuException(ResultCode.BAD_REQUEST, "존재하지 않는 토큰입니다."));
+  }
+
+  /**
+   * 사용자 지갑 주소로 token 전송
+   */
+  @Override
+  public Long purchase(String userWalletAddress, Long tokenId) {
+    Token token = getToken(tokenId);
+    nftService.transferNft(userWalletAddress, token.getNftId());
+    this.updatePaymentState(token);
+    return token.getId();
+  }
+
+  /**
+   * 토큰 paymentState를 SOLD_OUT으로 변경
+   */
+  @Override
+  @Transactional
+  public void updatePaymentState(Token token) {
+    token.updatePaymentState(PaymentState.SOLD_OUT);
+    tokenRepository.save(token);
+  }
+}
