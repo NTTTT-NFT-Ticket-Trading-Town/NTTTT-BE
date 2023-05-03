e6091c64fd6fed5b636e7f15d4026c996b60266c
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-04 00:41:16 +0900

    feat: swagger 설정 추가 및 간단한 Tag 추가

--- src/main/java/com/knu/ntttt_server/token/controller/TokenController.java
+++ src/main/java/com/knu/ntttt_server/token/controller/TokenController.java
@@ -3,10 +3,12 @@ package com.knu.ntttt_server.token.controller;
 import com.knu.ntttt_server.core.response.ApiResponse;
 import com.knu.ntttt_server.token.dto.TokenDto.QueryTokenRes;
 import com.knu.ntttt_server.token.service.TokenService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Token")
 @RestController
 public class TokenController {
 
