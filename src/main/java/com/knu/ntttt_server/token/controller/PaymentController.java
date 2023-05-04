092fb7da09df8f447ca26b55ac9aac5da1c0c293
kayoung0 <kkayoung0@gmail.com>
2023-05-04 14:27:58 +0900

    add: PaymentController 클래스에 paymentButton 추가
    결제 페이지에서 결제 버튼 클릭 시 PaymentService의 purchase method를 호출한다

---
+++ src/main/java/com/knu/ntttt_server/token/controller/PaymentController.java
@@ -0,0 +1,34 @@
+package com.knu.ntttt_server.token.controller;
+
+import com.knu.ntttt_server.core.exception.KnuException;
+import com.knu.ntttt_server.core.response.ApiResponse;
+import com.knu.ntttt_server.core.response.ResultCode;
+import com.knu.ntttt_server.token.service.PaymentService;
+import com.knu.ntttt_server.user.service.UserService;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PaymentController {
+
+  private final PaymentService paymentService;
+  private final UserService userService;
+
+  /**
+   * 결제 페이지에서 결제 버튼 클릭
+   */
+  @PostMapping("/payment")
+  public ApiResponse<?> paymentButton(@RequestBody Long tokenId, Principal principal) {
+    if (principal == null) {
+      return ApiResponse.error(new KnuException(ResultCode.BAD_REQUEST, "로그인을 해주세요"));
+    } else {
+      String nickname = principal.getName();
+      Long purchasedToken = paymentService.purchase(userService.getWalletAddress(nickname), tokenId);
+      return ApiResponse.ok(purchasedToken);
+    }
+  }
+}
