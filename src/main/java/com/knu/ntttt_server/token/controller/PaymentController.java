765989006f0ed9984b3f46c367b487237440e5b1
kayoung0 <kkayoung0@gmail.com>
2023-05-04 18:01:04 +0900

    fix: paymentButton의 requestBody를 변경
    @RequestBody Long id에서 @RequestBody Map<String,Long> param으로 수정

--- src/main/java/com/knu/ntttt_server/token/controller/PaymentController.java
+++ src/main/java/com/knu/ntttt_server/token/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.knu.ntttt_server.core.response.ResultCode;
 import com.knu.ntttt_server.token.service.PaymentService;
 import com.knu.ntttt_server.user.service.UserService;
 import java.security.Principal;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,12 +23,12 @@ public class PaymentController {
    * 결제 페이지에서 결제 버튼 클릭
    */
   @PostMapping("/payment")
-  public ApiResponse<?> paymentButton(@RequestBody Long tokenId, Principal principal) {
+  public ApiResponse<?> paymentButton(@RequestBody Map<String,Long> param, Principal principal) {
     if (principal == null) {
       return ApiResponse.error(new KnuException(ResultCode.BAD_REQUEST, "로그인을 해주세요"));
     } else {
       String nickname = principal.getName();
-      Long purchasedToken = paymentService.purchase(userService.getWalletAddress(nickname), tokenId);
+      Long purchasedToken = paymentService.purchase(userService.getWalletAddress(nickname), param.get("id"));
       return ApiResponse.ok(purchasedToken);
     }
   }
