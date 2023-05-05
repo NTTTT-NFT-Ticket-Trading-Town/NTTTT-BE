b37ec80755915a91d0c0100617be853ff98643e6
SangHyun-Park <51109514+PPakSang@users.noreply.github.com>
2023-05-05 15:31:32 +0900

    Fix: 모든 요청에 대한 인증을 수행하지 않도록 코드 변경 (#16)
    개발 환경에서 인증 단계 생략을 위해 기존 코드를 수정합니다

--- src/main/java/com/knu/ntttt_server/user/config/SecurityConfig.java
+++ src/main/java/com/knu/ntttt_server/user/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
                 .httpBasic().disable();
         return http.build();
     }
