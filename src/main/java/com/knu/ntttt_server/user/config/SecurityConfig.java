0b358ccffed1cb4aa86e3174fc496df97fc37d8c
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-05 12:43:51 +0900

    feat: 스웨거 설정 추가,  (#14)
    * modify: Build.gradle, application.yml 수정  for swagger 설정
    
    * modify: Datatsource url 일부 수정 for db naming
    
    * fix: springboot 3.X버전에 맞게 build.gradle수정
    
    기존에 업데이트한 의존성은 springboot 3.x버전에 적요되지 않는 의존성이라 수정함
    
    * feat: spring security login form disable을 위한 filterChain 추가
    
    * fix: EOF 추가
    
    * feat: swagger 설정 추가 및 간단한 Tag 추가
    
    * fix: 필요없는 의존성 (주석) 삭제

--- src/main/java/com/knu/ntttt_server/user/config/SecurityConfig.java
+++ src/main/java/com/knu/ntttt_server/user/config/SecurityConfig.java
@@ -2,11 +2,27 @@ package com.knu.ntttt_server.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/swagger-ui.html", "/api-docs");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+                .httpBasic().disable();
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
