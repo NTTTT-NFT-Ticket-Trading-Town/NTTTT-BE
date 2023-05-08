44cade2a72c9d94ecfa84d6a2ecfca74a45e24f9
kayoung0 <kkayoung0@gmail.com>
2023-05-08 13:14:14 +0900

    add: owner 필드 추가
    token 소유자의 지갑 주소

--- src/main/java/com/knu/ntttt_server/token/model/Token.java
+++ src/main/java/com/knu/ntttt_server/token/model/Token.java
@@ -46,8 +46,11 @@ public class Token {
   @NotNull
   private LocalDateTime publishedAt;
 
+  @NotNull
+  private String owner;
+
   @Builder
-  public Token(Event event, Integer seq, String imgUrl, Artist artist, Long price, String description, Long nftId, LocalDateTime publishedAt){
+  public Token(Event event, Integer seq, String imgUrl, Artist artist, Long price, String description, Long nftId, LocalDateTime publishedAt, String owner){
     this.event = event;
     this.seq = seq;
     this.imgUrl = imgUrl;
@@ -56,6 +59,7 @@ public class Token {
     this.description = description;
     this.nftId = nftId;
     this.publishedAt = publishedAt;
+    this.owner = owner;
   }
 
   public void soldOut() {
