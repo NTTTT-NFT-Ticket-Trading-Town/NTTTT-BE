427bc9ffeabf8638aa51d47651671e8f90753c68
kayoung0 <kkayoung0@gmail.com>
2023-05-09 13:20:34 +0900

    chore: Image 클래스의 imageUrl을 url로 변경

--- src/main/java/com/knu/ntttt_server/token/model/Image.java
+++ src/main/java/com/knu/ntttt_server/token/model/Image.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class Image {
-    public String imageUrl;
+    public String url;
     public String ratio;
 
-    public Image(String imageUrl, String ratio) {
-        this.imageUrl = imageUrl;
+    public Image(String url, String ratio) {
+        this.url = url;
         this.ratio = ratio;
     }
 }
