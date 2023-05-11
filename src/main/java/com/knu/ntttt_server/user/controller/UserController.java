001320ab0a2381cb6e3af447ac3add7c8fe8c3d7
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-11 17:16:41 +0900

    feat: 토큰 헬스체크 전용 url 추가

--- src/main/java/com/knu/ntttt_server/user/controller/UserController.java
+++ src/main/java/com/knu/ntttt_server/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.knu.ntttt_server.user.controller;
 
+import com.knu.ntttt_server.core.annotation.CurrentUser;
 import com.knu.ntttt_server.core.response.ApiResponse;
 import com.knu.ntttt_server.user.dto.LoginDto;
 import com.knu.ntttt_server.user.dto.UserDto;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User")
@@ -30,5 +32,15 @@ public class UserController {
     public ApiResponse<?> login(@RequestBody LoginDto dto) {
         String token = userService.login(dto);
         return ApiResponse.ok(token);
+    }
+
+    @Operation(summary = "토큰 health check")
+    @GetMapping("/token")
+    public ApiResponse<?> token(@CurrentUser User user) {
+        if (user != null) {
+            return ApiResponse.ok(user.getUsername());
+        }
+
+        return ApiResponse.ok();
     }
 }
