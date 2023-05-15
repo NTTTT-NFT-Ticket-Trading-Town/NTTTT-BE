3567f2696373b93dcb6e50396d97acbbcbef7b5f
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-15 16:04:48 +0900

    feat: ArtistRes 추가

--- src/main/java/com/knu/ntttt_server/token/dto/ArtistDto.java
+++ src/main/java/com/knu/ntttt_server/token/dto/ArtistDto.java
@@ -2,12 +2,28 @@ package com.knu.ntttt_server.token.dto;
 
 import com.knu.ntttt_server.token.model.Artist;
 import com.knu.ntttt_server.token.model.Group;
+import lombok.Builder;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ArtistDto {
+
+    @Builder
+    public record ArtistRes(Long id, Group group, String name, String imgUrl, String ratio) {
+        public static ArtistRes from(Artist artist) {
+            return ArtistRes.builder()
+                    .id(artist.getId())
+                    .group(artist.getGroup())
+                    .name(artist.getName())
+                    .imgUrl(artist.getImgUrl())
+                    .ratio(artist.getRatio())
+                    .build();
+        }
+    }
+
     public record ArtistsByGroup(Group group, List<Artist> members) {
         public static List<ArtistsByGroup> distribute(List<Artist> artists) {
             Map<Group, List<Artist>> map = new HashMap<>();
