7b900b67b5f778567d3c0a3511d3d9e786ad6cb7
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-15 10:35:45 +0900

    feat: user gacha token 엔티티 추가
    유저가 가챠를 돌린 토큰의 정보가 들어갑니다

---
+++ src/main/java/com/knu/ntttt_server/token/model/UserGachaToken.java
@@ -0,0 +1,32 @@
+package com.knu.ntttt_server.token.model;
+
+import com.knu.ntttt_server.user.model.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserGachaToken {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "token_id")
+    private Token token;
+
+    private LocalDate date;
+}
