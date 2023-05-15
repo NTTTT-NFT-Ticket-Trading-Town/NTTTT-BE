a0e3bd79413a2982d6cee7ee33e8a6a352e96730
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-16 00:37:39 +0900

    feat: 판매중인 토큰 중 특정 아티스트의 랜덤한 한 토큰 반환

--- src/main/java/com/knu/ntttt_server/token/repository/TokenRepository.java
+++ src/main/java/com/knu/ntttt_server/token/repository/TokenRepository.java
@@ -1,9 +1,16 @@
 package com.knu.ntttt_server.token.repository;
 
+import com.knu.ntttt_server.token.model.Artist;
 import com.knu.ntttt_server.token.model.Token;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     List<Token> queryAllByEvent_Id(Long id);
+
+    @Query(value = "SELECT * FROM token t WHERE t.artist_id = :artistId AND t.payment_state = 'ON_SALE' ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<Token> findRandomTokenByArtistIdAndPaymentStateOnSale(Long artistId);
 }
