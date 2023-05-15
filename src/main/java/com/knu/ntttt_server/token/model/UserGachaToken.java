4e444db1e87f0fb8d6c526e4dd8cab6882020793
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-16 00:13:55 +0900

    feat: playGacha 함수 추가 in UserGachaToken

--- src/main/java/com/knu/ntttt_server/token/model/UserGachaToken.java
+++ src/main/java/com/knu/ntttt_server/token/model/UserGachaToken.java
@@ -4,6 +4,7 @@ import com.knu.ntttt_server.user.model.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -35,4 +36,19 @@ public class UserGachaToken {
     private LocalDate date;
 
     private int chance = DEFAULT_CHANCE_NUMBER;
+
+    public UserGachaToken(User user, Token token) {
+        this.user = user;
+        this.token = token;
+    }
+
+    /**
+     * 새로운 랜덤 가챠 토큰으로 수정하고
+     * 기회를 1회 감소시킨다.
+     * @param token
+     */
+    public void playGacha(Token token) {
+        this.token = token;
+        this.chance--;
+    }
 }
