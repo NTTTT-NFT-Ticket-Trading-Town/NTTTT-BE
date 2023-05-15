fdfada1c34dd9f7813c3ecf8d114a9e133f72194
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-15 16:54:07 +0900

    feat: 토큰 솔드아웃, 토큰 존재안함 result code 추가

--- src/main/java/com/knu/ntttt_server/core/response/ResultCode.java
+++ src/main/java/com/knu/ntttt_server/core/response/ResultCode.java
@@ -13,7 +13,11 @@ public enum ResultCode {
      */
     SUCCESS(HttpStatus.OK, 200_001, "success"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500_001, "internal server error"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, 400_001, "cannot find token");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, 400_001, "cannot find token"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, 404_001, "not found"),
+    TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, 404_002, "토큰이 존재하지 않습니다."),
+    TOKEN_SOLD_OUT(HttpStatus.NOT_FOUND, 404_002, "토큰이 이미 팔렸습니다.");
+
     private HttpStatus httpStatus;
     private int code;
     private String message;
