d4b0e540b934a9a3879c90f94d504decd5f722fe
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-05-22 22:26:42 +0900

    fix: 이미 선택한 아티스트를 추가하려고할 때 예외처리 추가

--- src/main/java/com/knu/ntttt_server/core/response/ResultCode.java
+++ src/main/java/com/knu/ntttt_server/core/response/ResultCode.java
@@ -15,6 +15,7 @@ public enum ResultCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500_001, "internal server error"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, 400_001, "cannot find token"),
     GACHA_CHANCE_OVER(HttpStatus.BAD_REQUEST, 400_002, "일일 가챠 횟수를 초과했습니다."),
+    ARTIST_ALREADY_SELECT(HttpStatus.BAD_REQUEST, 400_003, "해당 아티스트를 이미 선택했습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, 404_001, "not found"),
     TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, 404_002, "토큰이 존재하지 않습니다."),
     TOKEN_SOLD_OUT(HttpStatus.NOT_FOUND, 404_002, "토큰이 이미 팔렸습니다.");
