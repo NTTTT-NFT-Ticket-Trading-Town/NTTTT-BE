7630d555bd9d1854ff1390b52d32ed884cc3d5d9
PPakSang <sanghyun-dev@naver.com>
2023-05-22 21:46:47 +0900

    Refactor: Controller 메소드 반환 타입 명시적으로 변환, 사용자 보유 토큰 및 선호 아티스트 정보 반환 타입 추가
    Swagger 에 반환 데이터 타입이 나올 수 있도록 메소드 반환 타입 명시적으로 변환

--- src/main/java/com/knu/ntttt_server/token/controller/TokenController.java
+++ src/main/java/com/knu/ntttt_server/token/controller/TokenController.java
@@ -2,10 +2,12 @@ package com.knu.ntttt_server.token.controller;
 
 import com.knu.ntttt_server.core.response.ApiResponse;
 import com.knu.ntttt_server.token.dto.TokenDto.TokenReq;
+import com.knu.ntttt_server.token.dto.TokenDto.TokenRes;
 import com.knu.ntttt_server.token.model.Token;
 import com.knu.ntttt_server.token.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Token")
@@ -23,13 +25,13 @@ public class TokenController {
    */
   @GetMapping(value="/token/{id}")
     @Operation(summary = "단일 토큰 조회", description = "단일 토큰을 조회합니다.")
-  public ApiResponse<?> findToken(@PathVariable("id") Long tokenId) {
+  public ApiResponse<TokenRes> findToken(@PathVariable("id") Long tokenId) {
     return ApiResponse.ok(tokenService.findBy(tokenId));
   }
 
   @GetMapping(value="/event/token/{eventId}")
   @Operation(summary = "이벤트의 모든 토큰 조회", description = "이벤트의 모든 토큰을 조회합니다.")
-  public ApiResponse<?> findTokensInEvent(@PathVariable Long eventId) {
+  public ApiResponse<List<TokenRes>> findTokensInEvent(@PathVariable Long eventId) {
     return ApiResponse.ok(tokenService.findAllBy(eventId));
   }
 
--- src/main/java/com/knu/ntttt_server/user/dto/UserArtistDto.java
+++ src/main/java/com/knu/ntttt_server/user/dto/UserArtistDto.java
@@ -1,9 +1,11 @@
 package com.knu.ntttt_server.user.dto;
 
+import com.knu.ntttt_server.token.dto.TokenDto.TokenRes;
 import com.knu.ntttt_server.token.model.Artist;
 import com.knu.ntttt_server.token.model.Group;
 import com.knu.ntttt_server.user.model.User;
 import com.knu.ntttt_server.user.model.UserArtist;
+import java.util.List;
 
 public class UserArtistDto {
 
@@ -19,6 +21,12 @@ public class UserArtistDto {
                 .build();
         }
     }
+
+    /**
+     * @param gachaList 유저가 보유하고 있는 토큰 리스트
+     * @param categoryList 유저가 선택한 아티스트 리스트
+     */
+    public record UserTokenArtistRes(List<TokenRes> gachaList, List<ChosenArtistRes> categoryList) {}
 
     public record ChosenArtistRes(String name, Group group) {
         public ChosenArtistRes(Artist artist) {
