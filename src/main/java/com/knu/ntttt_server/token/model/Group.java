b884e8dd66175362ba27f6fe994d1f815a6c1d07
PPakSang <sanghyun-dev@naver.com>
2023-05-23 18:49:27 +0900

    Add: Publisher, Group 데이터 추가 Publisher Mnet Group 르세라핌 추가

--- src/main/java/com/knu/ntttt_server/token/model/Group.java
+++ src/main/java/com/knu/ntttt_server/token/model/Group.java
@@ -1,9 +1,10 @@
 package com.knu.ntttt_server.token.model;
 
 public enum Group {
-    BTS(Publisher.HIVE), Aespa(Publisher.SM);
+    BTS(Publisher.HIVE), LE_SSERAFIM(Publisher.HIVE),
+    Aespa(Publisher.SM);
 
-    public Publisher publisher;
+    public final Publisher publisher;
 
     Group(Publisher publisher) {
         this.publisher = publisher;
--- src/main/java/com/knu/ntttt_server/token/model/Publisher.java
+++ src/main/java/com/knu/ntttt_server/token/model/Publisher.java
@@ -4,5 +4,6 @@ package com.knu.ntttt_server.token.model;
  * NFT 발행사
  */
 public enum Publisher {
-    HIVE, SM, JYP
+    HIVE, SM, JYP,
+    Mnet
 }
