58ef7a28e739e81135492f4a8c9b9ba710de8b03
PPakSang <sanghyun-dev@naver.com>
2023-05-24 21:06:00 +0900

    Feat: 같은 토큰을 구매중인 유저의 수 조회 로직 추가 가장 최근에 뽑은 토큰의 아이디를 통해 같은 토큰을 구매중인 유저의 수를 조회합니다.

--- src/main/java/com/knu/ntttt_server/token/repository/UserGachaTokenRepository.java
+++ src/main/java/com/knu/ntttt_server/token/repository/UserGachaTokenRepository.java
@@ -1,16 +1,15 @@
 package com.knu.ntttt_server.token.repository;
 
-import com.knu.ntttt_server.token.model.Artist;
-import com.knu.ntttt_server.token.model.PaymentState;
-import com.knu.ntttt_server.token.model.Token;
 import com.knu.ntttt_server.token.model.UserGachaToken;
 import com.knu.ntttt_server.user.model.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public interface UserGachaTokenRepository extends JpaRepository<UserGachaToken, Long> {
     Optional<UserGachaToken> findByUserAndDate(User user, LocalDate date);
+
+    List<UserGachaToken> findAllByToken_Id(Long tokenId);
 }
