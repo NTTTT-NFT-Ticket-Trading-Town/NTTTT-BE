5bd331dbc5f337c1db9cb9937933bf8f9e2eec7c
PPakSang <sanghyun-dev@naver.com>
2023-05-24 22:41:04 +0900

    Refactor: 관심 아티스트 수정을 위한 기존 아티스트 추가 로직 변경 가장 최신의 관심 아티스트 요청을 기준으로, 모든 관심 아티스트를 재설정하는 로직으로 변경합니다.

--- src/main/java/com/knu/ntttt_server/user/repository/UserArtistRepository.java
+++ src/main/java/com/knu/ntttt_server/user/repository/UserArtistRepository.java
@@ -19,4 +19,6 @@ public interface UserArtistRepository extends JpaRepository<UserArtist, Long> {
     
     @Query(value = "SELECT * FROM user_artist ut WHERE ut.user_id = :userId ORDER BY RAND() LIMIT 1", nativeQuery = true)
     Optional<UserArtist> findRandomUserArtistByUserId(Long userId);
+
+    void deleteAllByUser_Id(Long userId);
 }
